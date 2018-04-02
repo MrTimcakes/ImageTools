f41f8eeb3cdbc17be76c985a9c10ddb0f9d61c22
Timothy Adamson <MrTimcakes@gmail.com>
2018-04-02 03:30:20 +0100

    Script Metadata

---
+++ ImageTools.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Image Tools
+// @namespace   https://MrTimcakes.com/
+// @version     0.1
+// @author      MrTimcakes
+// @description Utilities For Embedded Images
+// @icon        https://cdn.rawgit.com/MrTimcakes/ImageTools/0e7c168d/Icon64.png
+// @updateURL   https://raw.githubusercontent.com/MrTimcakes/ImageTools/master/ImageTools.user.js
+// @downloadURL https://raw.githubusercontent.com/MrTimcakes/ImageTools/master/ImageTools.user.js
+// @supportURL  https://github.com/MrTimcakes/ImageTools/issues
+// @match       *
+// @run-at      document-end
+// @copyright   2018 MrTimcakes
+// ==/UserScript==
